e004286dedf35105589da062d7a8ee5b2a6d2124
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-08-04 11:07:30 -0400

    Added request authentication

--- Keys_Backend/server/src/index.ts
+++ Keys_Backend/server/src/index.ts
@@ -4,17 +4,20 @@ require("dotenv").config()
 import {redisSession} from './Redis/RedisInit';
 import './Mongo/MongoInit'
 import * as routes from './Routes'
-import updateKDBX from './Routes/UpdateKDBX';
+import cookieParser from 'cookie-parser'
+import AuthenticateRequest from './Middleware/AuthenticateRequest';
 
-const app: Application = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app: Application = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(AuthenticateRequest)
 app.use(redisSession)
-
-app.listen(process.env.NODE_SERVER_PORT!)
 
 app.post('/login', routes.login)
 app.post('/createAccount', routes.createAccount)
 app.post('/updateKDBX', routes.updateKDBX)
 app.get('/getKDBX', routes.getKDBX)
+
+app.listen(process.env.NODE_SERVER_PORT!)
